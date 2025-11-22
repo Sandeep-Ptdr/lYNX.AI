f66e3103df29c36bbdcb9c29f5b4a4f2c67a663c
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-11-22 15:37:38 +0530

    feat(layout): enhance RootLayout by adding Providers and Toaster components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ModeProvider } from "@/context/modeContext";
+ 
+import Providers from "./providers/provider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +23,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ModeProvider>
-          {children}
-        </ModeProvider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+          <ModeProvider>{children}</ModeProvider>
+          <Toaster/>
+        </Providers>
       </body>
     </html>
   );
