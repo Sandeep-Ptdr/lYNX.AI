e735c27d4ea1eb8c16b0ce5a61806a29dd2380e6
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-11-22 15:38:19 +0530

    refactor(auth): replace connectDb with MongoDB client for user authorization

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
-import { connectDb } from "@/lib/connectDb";
+
+
+
 import clientPromise from "@/lib/mongodb";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
-
-import User from "@/app/api/models/user";
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 
@@ -19,22 +19,34 @@ export const authOptions = {
       credentials: {},
 
       async authorize(credentials) {
-        await connectDb();
 
-        const user = await User.findOne({ email: credentials.email });
+       
+        const client = await clientPromise;
+        const db = client.db();
+
+        
+        const usersCollection = db.collection("users");
+
+         
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
         if (!user) {
-           throw new Error("User not found!");
+          throw new Error("User not found!");
         }
-
+ 
         const isMatch = await bcrypt.compare(
           credentials.password,
           user.password
         );
+
         if (!isMatch) {
-           throw new Error("Invalid credentials");
+          throw new Error("Invalid credentials");
         }
+
+         
         return {
-           
           id: user._id.toString(),
           name: user.name,
           email: user.email,
@@ -47,15 +59,19 @@ export const authOptions = {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
+        token.name = user.name;
       }
       return token;
     },
 
     async session({ session, token }) {
       session.user.id = token.id;
+      session.user.name = token.name;
+
       return session;
     },
   },
+
   pages: {
     signIn: "/login",
   },
