dff99306da9395e971e086d1459b54b9d9d68bc2
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-11-22 15:40:07 +0530

    fix(connectDb): remove redundant variable declaration and improve error handling

--- src/lib/connectDb.js
+++ src/lib/connectDb.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
 export async function connectDb() {
   try {
-    let isConnected = false;
     if (isConnected) return;
     const db = await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
-    console.log(db.connection._connectionString, "connected");
+    return db;
+    // console.log(db.connection._connectionString, "connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    throw error;
+    return { success: false, message: "Error connecting..." };
   }
 }
