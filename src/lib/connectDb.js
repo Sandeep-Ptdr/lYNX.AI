59c6c87e0f921c03eb3128d47a8ed2a1a35ac9fc
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-11-26 15:37:51 +0530

    feat(db): enhance connectDb function with error handling and connection validation

--- src/lib/connectDb.js
+++ src/lib/connectDb.js
@@ -1,15 +1,45 @@
+// import mongoose from "mongoose";
+
+// let isConnected = false;
+// export async function connectDb() {
+//   try {
+//     if (isConnected) return;
+//     const db = await mongoose.connect(process.env.MONGODB_URI);
+//     isConnected = true;
+//     return db;
+//     // console.log(db.connection._connectionString, "connected");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     return { success: false, message: "Error connecting..." };
+//   }
+// }
+
+
+
+
 import mongoose from "mongoose";
 
 let isConnected = false;
+
 export async function connectDb() {
+  if (isConnected) return;
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error("Missing MONGODB_URI");
+  }
+
   try {
-    if (isConnected) return;
-    const db = await mongoose.connect(process.env.MONGODB_URI);
-    isConnected = true;
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "test",//db name
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+
+    console.log("MongoDB Connected");
+
     return db;
-    // console.log(db.connection._connectionString, "connected");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    return { success: false, message: "Error connecting..." };
+    console.error("MongoDB Connection Error:", error.message);
+    throw new Error("Error connecting to MongoDB");
   }
 }
