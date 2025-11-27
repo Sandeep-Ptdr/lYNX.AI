4c2578ae470ef348bf3f492f2572e37e56de0e37
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-11-27 17:24:59 +0530

    feat(sidebar): enhance chat sidebar with delete functionality and new chat button

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,15 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import {
-  User2,
-  ChevronUp,
-  Calendar,
-  Home,
-  Inbox,
-  Search,
-  Settings,
-} from "lucide-react";
+import { User2, ChevronUp, Trash2, Plus, Edit } from "lucide-react";
 
 import {
   Sidebar,
@@ -29,37 +21,83 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
+import { Button } from "@/components/ui/button";
 import LogoutBtn from "./LogoutBtn";
-
-// Menu items
-const items = [
-  { title: "Home", url: "#", icon: Home },
-  { title: "Inbox", url: "#", icon: Inbox },
-  { title: "Calendar", url: "#", icon: Calendar },
-  { title: "Search", url: "#", icon: Search },
-  { title: "Settings", url: "#", icon: Settings },
-];
+import { useChatBot } from "@/hooks/useChat";
+import { usePathname, useRouter } from "next/navigation";
 
 export function AppSidebar() {
   const { data: session } = useSession();
 
   const username = session?.user?.name || "User";
 
+  const {
+    conversations,
+    errorMessages,
+    convLoading,
+    refreshConversations,
+    deleteConversation,
+  } = useChatBot();
+
+  const loading = convLoading || errorMessages.length > 0;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  // console.log("cpnverattins", conversations);
+
+  const handleDelete = async (conversationId) => {
+    const deleted = await deleteConversation(conversationId);
+    if (!deleted) return;
+
+    refreshConversations();
+
+    if (pathname.includes(conversationId)) {
+      router.push("/chat/new");
+    }
+  };
+
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
+          <SidebarGroupContent className="mt-4">
+            {/* LOGO LEFT ALIGNED */}
+            <div
+              className="flex items-center gap-3 mb-6 pl-2 cursor-pointer"
+              onClick={() => router.push("/chat/new")}
+            >
+              {/* <img
+                src="/logo.png"
+                alt="LYNX Logo"
+                className="h-10 w-10 object-contain"
+              /> */}
+              <span className="font-bold text-xl tracking-wide">LYNX</span>
+            </div>
+
+            <Button
+              onClick={() => router.push("/chat/new")}
+              className="rounded-[4px] cursor-pointer"
+              variant="ghost"
+            >
+              <Edit /> New Chat
+            </Button>
+            <SidebarMenu className="mt-4">
+              <SidebarGroupLabel className="text-lg ">Chats</SidebarGroupLabel>
+              {conversations?.map((item, index ) => (
+                <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild className="rounded-[6px]  ">
+                    <div className="flex justify-between items-center">
+                      <a className="w-full" href={`/chat/${item?._id}`}>
+                        <span>{item.title}</span>
+                      </a>
+                      <button
+                        tooltip="Delete"
+                        className="cursor-pointer hidden group-hover/menu-item:inline-block transform hover:scale-110 transition-transform duration-200 ease-in-out"
+                        onClick={() => handleDelete(item?._id)}
+                      >
+                        <Trash2 size={17} />
+                      </button>
+                    </div>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
