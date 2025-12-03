aaf05a8a548544a54d74fd178019d806e7574517
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-12-03 17:00:38 +0530

    fix(auth): update public routes in middleware to include home path

--- src/middleware.js
+++ src/middleware.js
@@ -57,7 +57,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-const authRoutes = ["/login", "/register"]; // Public pages
+const authRoutes = ["/","/login", "/register"]; // Public pages
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
